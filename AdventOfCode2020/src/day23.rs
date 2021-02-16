3fbd124a1eaf622a5b4ab3a0bfbff3a5f6de8c00
Alex Utter <ooterness@gmail.com>
2021-02-15 17:08:45 -0800

    Rewrite Day 23 solution using linked-lists.

--- AdventOfCode2020/src/day23.rs
+++ AdventOfCode2020/src/day23.rs
@@ -1,88 +1,68 @@
 /// Day 23: https://adventofcode.com/2020/day/23
 /// Copyright 2021 by Alex Utter
 
-use std::cmp::min;
 #[path = "common.rs"] mod common;
 
 struct Cups {
-    curr: usize,        // Current index
-    cups: Vec<usize>,   // Label for cup by index
-    posn: Vec<usize>,   // Position of cup by label
+    len:  usize,        // Number of cups
+    curr: usize,        // Label for current cup
+    next: Vec<usize>,   // Label of next cup, indexed by label
 }
 
 impl Cups {
     /// Create initial state from string.
     fn new(s: &str, len:usize) -> Cups {
-        // Initialize labels and positions 0 through N-1.
-        let mut cups: Vec<usize> = (0..len).collect();
-        let mut posn: Vec<usize> = (0..len).collect();
+        // Initialize circular linked list for positions 0 through N-1.
+        let mut next: Vec<usize> = (0..len).map(|n| (n+1) % len).collect();
         // Overwrite initial state as dictated by string.
+        let mut start = 0usize;
+        let mut prev = len - 1;
         for (n,c) in String::from(s).chars().enumerate() {
             let d = (c.to_digit(10).unwrap() - 1) as usize;
-            cups[n] = d;
-            posn[d] = n;
+            if n == 0 {start = d;}  // Index of first cup
+            next[prev] = d;         // Link previous cup to this one
+            prev = d;
         }
-        Cups {curr:0, cups:cups, posn:posn}
+        // Special case for end of initial state...
+        if s.len() < len {
+            next[prev] = s.len();   // Link to rest of list
+        } else {
+            next[prev] = start;     // Wrap to beginning
+        }
+        // Construct the complete object.
+        Cups {len:len, curr:start, next:next}
     }
 
-    /// Fetch number of cups.
-    fn len(&self) -> usize {
-        self.cups.len()
-    }
-
-    /// Fetch index of Nth cup from current position.
-    fn idx(&self, offset:usize) -> usize {
-        (self.curr + offset) % self.len()
-    }
-
-    /// Fetch value of Nth cup from current position.
-    fn val(&self, offset:usize) -> usize {
-        self.cups[self.idx(offset)]
-    }
-
-    /// Given current state, find destination index.
+    /// Given current state, find destination label.
     fn dest(&self) -> usize {
         // Initial state: "pick up" next three cups.
-        let cur = self.val(0);
-        let up1 = self.val(1);
-        let up2 = self.val(2);
-        let up3 = self.val(3);
+        let cur = self.curr;
+        let up1 = self.next[cur];
+        let up2 = self.next[up1];
+        let up3 = self.next[up2];
         // Calculate the destination label.
-        let decr = |n| if n > 0 {n-1} else {self.len()-1};
+        let decr = |n| if n > 0 {n-1} else {self.len-1};
         let mut dst = decr(cur);
         while (dst == up1) || (dst == up2) || (dst == up3) {
             dst = decr(dst);
         }
-        // Return index relative to current position.
-        (self.posn[dst] + self.len() - self.curr) % self.len()
+        dst
     }
 
     fn iter_1(&mut self) {
-        // Find the destination position.
-        let rel = self.dest();
-        // Note value of the three cups being picked up.
-        let val1 = self.val(1);
-        let val2 = self.val(2);
-        let val3 = self.val(3);
-        let pos1 = self.idx(rel-2);
-        let pos2 = self.idx(rel-1);
-        let pos3 = self.idx(rel-0);
-        // From that point to destination, move cups foward three steps.
-        for n in 4..=rel {
-            let pos = self.idx(n-3);    // New position
-            let val = self.val(n);      // Label / value
-            self.cups[pos] = val;
-            self.posn[val] = pos;
-        }
-        // Deposit the cups that were picked up.
-        self.cups[pos1] = val1;
-        self.cups[pos2] = val2;
-        self.cups[pos3] = val3;
-        self.posn[val1] = pos1;
-        self.posn[val2] = pos2;
-        self.posn[val3] = pos3;
+        // Find the destination label.
+        let dst = self.dest();
+        // Note label of the first four cups.
+        let cur = self.curr;
+        let up1 = self.next[cur];
+        let up2 = self.next[up1];
+        let up3 = self.next[up2];
+        // Update the linked-list.
+        self.next[cur] = self.next[up3];
+        self.next[up3] = self.next[dst];
+        self.next[dst] = up1;
         // Update current cup index.
-        self.curr = self.idx(1);
+        self.curr = self.next[cur];
     }
 
     fn iter_n(&mut self, iter:usize) {
@@ -90,20 +70,17 @@ impl Cups {
     }
 
     fn product(&self) -> u64 {
-        let start = self.posn[0];
-        let cup1 = self.cups[(start+1) % self.len()] as u64;
-        let cup2 = self.cups[(start+2) % self.len()] as u64;
-        (cup1+1) * (cup2+1)
+        let cup1 = self.next[0];
+        let cup2 = self.next[cup1];
+        (cup1 as u64 + 1u64) * (cup2 as u64 + 1u64)
     }
 
     fn to_string(&self) -> String {
-        let start = self.posn[0];
-        let maxln = min(self.len(), 9usize);
         let mut result = String::new();
-        for n in 1..maxln {
-            let idx = (start + n) % self.cups.len();
-            let val = self.cups[idx] + 1usize;
-            result += &val.to_string();
+        let mut next = self.next[0];
+        for _ in 1..9 {
+            result += &(next+1).to_string();
+            next = self.next[next];
         }
         result
     }
