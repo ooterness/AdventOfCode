a3cb2887a0afa3bb8cd2def1301a2e0322f58c2f
Alex Utter <ooterness@gmail.com>
2021-12-11 00:05:49 -0800

    Refactor grid/matrix functions from Day 9 into a new set of reusable classes.

--- AdventOfCode2021/src/day09.rs
+++ AdventOfCode2021/src/day09.rs
@@ -2,66 +2,56 @@
 /// Copyright 2021 by Alex Utter
 
 #[path = "common.rs"] mod common;
+#[path = "grid.rs"] mod grid;
 use std::collections::HashSet;
+use grid::RowCol;
 
-type Height = u32;
+type Height = u8;
 struct HeightMap {
-    h: Vec<Vec<Height>>,    // Row then column
+    h: grid::Grid<Height>,
 }
 
 impl HeightMap {
     fn new(filename: &str) -> HeightMap {
-        let lines = common::read_lines(filename);
-        let height = lines.iter()   // For each line...
-            .map(|x| x.trim())      // Trim whitespace
-            .map(|x| x.chars()      // Parse each digit
-                .map(|d| d.to_digit(10).unwrap_or(10))
-                .collect());
-        HeightMap { h: height.collect() }
+        HeightMap { h: grid::read_grid(filename) }
     }
 
     // Fetch map height with "10" for out-of-bounds
-    fn get(&self, r: i32, c: i32) -> Height {
-        if (0 <= r) && ((r as usize) < self.h.len()) &&
-           (0 <= c) && ((c as usize) < self.h[r as usize].len())
-           {self.h[r as usize][c as usize]} else {10}
+    fn get(&self, rc: &RowCol) -> Height {
+        *self.h.get(rc).unwrap_or(&10)
     }
 
     // Is a given location a "low-point" per Part-1 description?
-    fn is_low(&self, r: i32, c: i32) -> bool {
-        (self.get(r,c) < self.get(r-1,c)) &&
-        (self.get(r,c) < self.get(r+1,c)) &&
-        (self.get(r,c) < self.get(r,c-1)) &&
-        (self.get(r,c) < self.get(r,c+1))
+    fn is_low(&self, rc: &RowCol) -> bool {
+        (self.get(rc) < self.get(&rc.nn())) &&
+        (self.get(rc) < self.get(&rc.ee())) &&
+        (self.get(rc) < self.get(&rc.ss())) &&
+        (self.get(rc) < self.get(&rc.ww()))
     }
 
-    fn risk(&self, r: i32, c: i32) -> u32 {
-        if self.is_low(r,c) {self.get(r,c) + 1} else {0}
+    fn risk(&self, rc: &RowCol) -> Height {
+        if self.is_low(rc) {self.get(rc) + 1} else {0}
     }
 
     // Find the Part-1 "risk level".
-    fn part1(&self) -> Height {
-        let rows = self.h.len() as i32;
-        let cols = self.h[0].len() as i32;
-        (0..rows).map(|r|
-            (0..cols).map(|c| self.risk(r,c))
-                .sum::<Height>())
+    fn part1(&self) -> u64 {
+        self.h.iter()
+            .map(|rc| self.risk(&rc) as u64)
             .sum()
     }
 
     // Find the size of a given basin using flood-fill.
-    fn basin(&self, r: i32, c: i32) -> Option<u64> {
+    fn basin(&self, start: RowCol) -> Option<u64> {
         // Only start from the lowest point in the basin.
-        if !self.is_low(r,c) {return None;}
+        if !self.is_low(&start) {return None;}
         // Otherwise start breadth-first search.
-        type Coord = (i32, i32);
-        let mut visited = HashSet::<Coord>::new();
-        let mut queue = Vec::<Coord>::new();
-        visited.insert((r,c));
-        queue.push((r,c));
+        let mut visited = HashSet::<RowCol>::new();
+        let mut queue = Vec::<RowCol>::new();
+        visited.insert(start);
+        queue.push(start);
         while let Some(rc) = queue.pop() {
-            for next in [(rc.0-1,rc.1),(rc.0+1,rc.1),(rc.0,rc.1-1),(rc.0,rc.1+1)] {
-                if self.get(next.0, next.1) < 9 && !visited.contains(&next) {
+            for next in [rc.nn(), rc.ee(), rc.ss(), rc.ww()] {
+                if self.get(&next) < 9 && !visited.contains(&next) {
                     visited.insert(next);
                     queue.push(next);
                 }
@@ -74,11 +64,9 @@ impl HeightMap {
     fn part2(&self) -> u64 {
         // Find all the basins...
         let mut basins = Vec::<u64>::new();
-        for r in 0..self.h.len() {
-            for c in 0..self.h[r].len() {
-                if let Some(x) = self.basin(r as i32, c as i32) {
-                    basins.push(x);
-                }
+        for rc in self.h.iter() {
+            if let Some(x) = self.basin(rc) {
+                basins.push(x);
             }
         }
         // Identify the largest three.
---
+++ AdventOfCode2021/src/grid.rs
@@ -0,0 +1,120 @@
+/// Common grid and matrix library functions for my Advent of Code solutions
+/// Copyright 2021 by Alex Utter
+
+#[path = "common.rs"] mod common;
+
+/// Read an MxN single-digit grid as a vector-of-vectors.
+#[allow(dead_code)]
+pub fn read_grid<T: From<u8>>(filename: &str) -> Grid<T> {
+    let lines = common::read_lines(filename);
+    let rows = lines.iter()     // For each line...
+        .map(|x| x.trim())      // Trim whitespace
+        .map(|x| x.chars()      // Parse each digit
+            .map(|d| d.to_digit(10).unwrap() as u8)
+            .map(|n| T::from(n))
+            .collect());
+    Grid::<T>::new(rows.collect())
+}
+
+/// Size of an RxC grid.
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub struct GridSize {
+    pub r: usize,
+    pub c: usize,
+}
+
+/// A "grid" is a matrix of values (rows then columns).
+pub struct Grid<T> {
+    pub data: Vec<Vec<T>>,
+    pub size: GridSize,
+}
+
+#[allow(dead_code)]
+impl<T> Grid<T> {
+    pub fn new(data: Vec<Vec<T>>) -> Grid<T> {
+        // Measure size using the first row.
+        let rows = data.len();
+        let cols = if rows > 0 {data[0].len()} else {0};
+        // Confirm every row has the same length.
+        for row in data.iter() {assert_eq!(row.len(), cols);}
+        Grid {data:data, size: GridSize{r:rows,c:cols} }
+    }
+
+    pub fn get(&self, rc: &RowCol) -> Option<&T> {
+        if (0 <= rc.r) && ((rc.r as usize) < self.size.r) &&
+           (0 <= rc.c) && ((rc.c as usize) < self.size.c) {
+            Some(&self.data[rc.r as usize][rc.c as usize])
+        } else {None}
+    }
+
+    pub fn gets(&self, rc: &GridSize) -> Option<&T> {
+        if (rc.r < self.size.r) && (rc.c < self.size.c) {
+            Some(&self.data[rc.r][rc.c])
+        } else {None}
+    }
+
+    pub fn iter<'a>(&'a self) -> GridIterator<'a> {
+        GridIterator { size:&self.size, next: GridSize{r:0,c:0} }
+    }
+}
+
+/// Iterator over every row/column coordinate in a Grid.
+pub struct GridIterator<'a> {
+    size: &'a GridSize,
+    next: GridSize,
+}
+
+impl<'a> Iterator for GridIterator<'a> {
+    type Item = RowCol;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next.r < self.size.r && self.next.c < self.size.c {
+            let result = Some( RowCol {
+                r:self.next.r as i32,
+                c:self.next.c as i32,
+            } );
+            if self.next.c + 1 < self.size.c {
+                self.next.c += 1;   // Move to next column
+            } else {
+                self.next.c  = 0;   // Wrap to next row
+                self.next.r += 1;
+            }
+            result
+        } else {None}
+    }
+}
+
+/// Abstract row/column position (often within a Grid).
+#[allow(dead_code)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub struct RowCol {
+    pub r: i32,
+    pub c: i32,
+}
+
+#[allow(dead_code)]
+impl RowCol {
+    pub fn new(r: i32, c: usize) -> RowCol {
+        RowCol {r:r as i32, c:c as i32}
+    }
+
+    // Move one space in a given direction
+    pub fn nw(&self) -> RowCol { self.nw_by(1) }
+    pub fn nn(&self) -> RowCol { self.nn_by(1) }
+    pub fn ne(&self) -> RowCol { self.ne_by(1) }
+    pub fn ee(&self) -> RowCol { self.ee_by(1) }
+    pub fn se(&self) -> RowCol { self.se_by(1) }
+    pub fn ss(&self) -> RowCol { self.ss_by(1) }
+    pub fn sw(&self) -> RowCol { self.sw_by(1) }
+    pub fn ww(&self) -> RowCol { self.ww_by(1) }
+
+    // Move N spaces in a given direction
+    pub fn nw_by(&self, d:i32) -> RowCol { RowCol {r:self.r-d, c:self.c-d} }
+    pub fn nn_by(&self, d:i32) -> RowCol { RowCol {r:self.r-d, c:self.c  } }
+    pub fn ne_by(&self, d:i32) -> RowCol { RowCol {r:self.r-d, c:self.c+d} }
+    pub fn ee_by(&self, d:i32) -> RowCol { RowCol {r:self.r,   c:self.c+d} }
+    pub fn se_by(&self, d:i32) -> RowCol { RowCol {r:self.r+d, c:self.c+d} }
+    pub fn ss_by(&self, d:i32) -> RowCol { RowCol {r:self.r+d, c:self.c  } }
+    pub fn sw_by(&self, d:i32) -> RowCol { RowCol {r:self.r+d, c:self.c-d} }
+    pub fn ww_by(&self, d:i32) -> RowCol { RowCol {r:self.r,   c:self.c-d} }
+}
