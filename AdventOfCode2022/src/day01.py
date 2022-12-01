2aaa82191597563652b23fa87a681d276d0da50d
Alex Utter <ooterness@gmail.com>
2022-12-01 07:58:29 -0800

    Day 1 part 2.

--- AdventOfCode2022/src/day01.py
+++ AdventOfCode2022/src/day01.py
@@ -13,8 +13,14 @@ def read_input(filename):
 def most_calories(elves):
     return max([sum(elf) for elf in elves])
 
+def top_three(elves):
+    top = sorted([sum(elf) for elf in elves])
+    return sum(top[-3:])
+
 if __name__ == '__main__':
     test = read_input('../input/test01.txt')
     input = read_input('../input/input01.txt')
     assert (most_calories(test) == 24000)
+    assert (top_three(test) == 45000)
     print(f'Part 1: {most_calories(input)}')
+    print(f'Part 2: {top_three(input)}')
