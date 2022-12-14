c6e7a23f6eaeaba381ec0371e0ee154ad110aa99
Alex Utter <ooterness@gmail.com>
2022-12-13 22:54:58 -0800

    2018 Day 16 Part 2

--- AdventOfCode2018/day16.py
+++ AdventOfCode2018/day16.py
@@ -83,20 +83,67 @@ def eqrr(a, b, c, reg):
 
 ALL_INSTRUCTIONS = [addr, addi, mulr, muli, banr, bani, borr, bori,
                     setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+ALL_OPCODES = range(len(ALL_INSTRUCTIONS))
 
-'''Count potentially-matching opcodes in a test.'''
-def count_instr(before, opcode, after):
-    (op, a, b, c) = opcode
+'''Check whether a given test could match each instruction.'''
+def check_instr(before, instr, after):
+    (op, a, b, c) = instr
     test = lambda fn: fn(a, b, c, before) == after
-    return sum([test(fn) for fn in ALL_INSTRUCTIONS])
+    return [test(fn) for fn in ALL_INSTRUCTIONS]
+
+'''Element-wise AND of a check matrix.'''
+def all_rows(opcode, check):
+    ok = [True] * len(ALL_INSTRUCTIONS)
+    for (op,chk) in check:
+        if op == opcode: ok = [ok[n] and chk[n] for n in ALL_OPCODES]
+    return ok
+
+'''Count the number of legal pairings in a truth matrix.'''
+def count_pairs(match):
+    return sum([sum(row) for row in match])
+
+'''Sudoku-style logical identification and cross-eliminations.'''
+def sudoku_rows(match, map):
+    for rr in ALL_OPCODES:
+        if rr in map.keys(): continue   # Already solved?
+        row = [match[rr][c] for c in ALL_OPCODES]
+        if sum(row) == 1:               # Unique ID?
+            cc = row.index(True)
+            map[rr] = ALL_INSTRUCTIONS[cc]
+            for r in ALL_OPCODES:       # Eliminations?
+                if r != rr: match[r][cc] = False
+            return True
+    return False
+
+def sudoku_cols(match, map):
+    for cc in ALL_OPCODES:
+        if ALL_INSTRUCTIONS[cc] in map.values(): continue
+        col = [match[r][cc] for r in ALL_OPCODES]
+        if sum(col) == 1:               # Unique ID?
+            rr = col.index(True)
+            map[rr] = ALL_INSTRUCTIONS[cc]
+            for c in ALL_OPCODES:       # Eliminations?
+                if c != cc: match[rr][c] = False
+            return True
+    return False
 
 '''Identify which opcode is which function, returning a dictionary.'''
-def identify_instr(test):
-    None #???
+def identify_instr(test, verbose=False):
+    # First check the legality for each test instruction.
+    check = [(i[0],check_instr(b,i,a)) for (b,i,a) in test]
+    # Check matching test vectors for each numeric opcode.
+    # (Each row is a numeric opcode, each column is an instruction.)
+    match = [all_rows(opcode, check) for opcode in ALL_OPCODES]
+    # Sudoku-style elimination until we find a unique solution.
+    map = {}
+    if verbose: print(count_pairs(match))
+    while sudoku_rows(match, map) or sudoku_cols(match, map):
+        if verbose: print(count_pairs(match))
+    return map
 
 '''How many test instructions could be more than three opcodes?'''
 def part1(test, prog):
-    return sum([count_instr(*t) >= 3 for t in test])
+    return sum([sum(check_instr(*t)) >= 3 for t in test])
 
 '''Work out which opcode is which, then run program.'''
 def part2(test, prog):
@@ -118,4 +165,4 @@ if __name__ == '__main__':
     input = read_input(get_data(day=16, year=2018))
     assert(part1(*test) == 1)
     print(f'Part 1: {part1(*input)}')
-    #print(f'Part 2: {part2(*input)}')
+    print(f'Part 2: {part2(*input)}')
