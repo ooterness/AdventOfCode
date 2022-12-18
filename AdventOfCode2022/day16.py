0ee1bc6cd632b61e8c89d9836b36f12238d0fc79
Alex Utter <ooterness@gmail.com>
2022-12-17 17:11:47 -0800

    2022 Day 16: Part 2 WIP

--- AdventOfCode2022/day16.py
+++ AdventOfCode2022/day16.py
@@ -17,39 +17,47 @@ def read_line(line, labels):
 
 def read_input(input):
     # Two passes: Labels only, then room parameters.
+    # (Sorting both lists ensures room 'AA' is at index zero.)
     labels = sorted([line[6:8] for line in input.splitlines()])
     rooms = [read_line(line, labels) for line in input.splitlines()] 
     return sorted(rooms)
 
-def dijkstra(rooms, verbose=False):
-    dist = {}               # For each time/room/vmask, maximum pressure released
-    next = [(-30,0,0,0)]    # Min-heap for search queue. (time, cost, node, valves)
+def dijkstra(rooms, start, verbose=False):
+    # Agents take turns, N turns = one minute.
+    agents = len(start)                     # Number of agents = Length of initial state
+    max_time = 30 if agents == 1 else 52    # 30 minutes = 30 ticks, or 26 minutes = 52 ticks
+    dist = {}                               # For each time/rooms/vmask, maximum pressure released
+    next = [(-max_time,0,start,0)]          # Min-heap for search queue. (time, cost, nodes, valves)
     while len(next) > 0:
-        (time,cost,node,valve) = heappop(next)  # Next state to consider?
+        (time,cost,nodes,valve) = heappop(next) # Next state to consider?
         if time >= 0: break                     # Out of time? Better solution?
-        if cost > dist.get((time,node,valve), 0): continue
-        if verbose: print((-time,-cost,node,valve))
+        if cost > dist.get((time,nodes,valve), 0): continue
+        if verbose: print((-time,-cost,nodes,valve))
+        agent = time % agents                   # Who's moving this tick?
         time += 1
-        (lbl,rate,outlets) = rooms[node]        # Lookup current room
+        trem = -time // agents                  # Minutes remaining (round down)
+        node = nodes[agent]
+        (lbl,rate,outlets) = rooms[node]        # Lookup current agent/room
         vmask = 2**node
         if (rate > 0) and not (valve & vmask):  # Try opening valve?
-            new_state = (time, node, valve|vmask)
-            new_cost = cost + time * rate
+            new_state = (time, nodes, valve|vmask)
+            new_cost = cost - trem * rate
             if new_cost < dist.get(new_state, 0):
                 dist[new_state] = new_cost
-                heappush(next, (time,new_cost,node,valve|vmask))
+                heappush(next, (time,new_cost,nodes,valve|vmask))
         for move in outlets:                    # Try each tunnel?
-            new_state = (time, move, valve)
+            new_move = tuple([move if n==agent else nodes[n] for n in range(agents)])
+            new_state = (time, new_move, valve)
             if cost < dist.get(new_state, 1):
                 dist[new_state] = cost
-                heappush(next, (time,cost,move,valve))
+                heappush(next, (time,cost,new_move,valve))
     return dist
 
 def part1(rooms):
-    return -min(dijkstra(rooms).values())
+    return -min(dijkstra(rooms, (0,)).values())
 
 def part2(rooms):
-    None
+    return -min(dijkstra(rooms, (0,0)).values())
 
 TEST = \
 '''
@@ -69,8 +77,7 @@ if __name__ == '__main__':
     test = read_input(TEST.strip())
     input = read_input(get_data(day=16, year=2022))
     assert(part1(test) == 1651)
-    #assert(part2(test) == 29)
+    assert(part2(test) == 1707)
     print(f'Part 1: {part1(input)}')
+    # TODO: Improved algorithm? No result after a full hour.
     #print(f'Part 2: {part2(input)}')
-    #??? 2285 = Too high
-    #??? 2484 = Too high
