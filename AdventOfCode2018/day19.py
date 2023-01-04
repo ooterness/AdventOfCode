7d8ec119a7a6d586c081cf71d0abbda8fd4cde70
Alex Utter <ooterness@gmail.com>
2023-01-04 00:54:36 -0800

    2018 Day 19: Part 2

--- AdventOfCode2018/day19.py
+++ AdventOfCode2018/day19.py
@@ -3,7 +3,7 @@
 
 from aocd import get_data
 from copy import deepcopy
-from opcodes import ALL_INSTRUCTIONS
+from opcodes import ALL_INSTRUCTIONS, store
 import re
 
 def read_input(input):
@@ -16,6 +16,21 @@ def read_input(input):
             (a, b, c) = [int(x) for x in re.findall('[0-9]+', line)]
             prog.append((op, a, b, c))
     return (iptr, prog)
+
+# Define some new psuedo-instructions.
+def noop(a, b, c, reg):
+    return reg
+def modr(a, b, c, reg):
+    if a >= len(reg) or b >= len(reg): return None
+    div = (reg[b] % reg[a] == 0)    # Is RegA a factor of RegB?
+    return store(reg[b] if div else 0, c, reg)
+
+def optimize(iptr, prog):
+    # The problem input has a nested loop that checks if a given
+    # register divides evenly into another register.  We replace
+    # the inner loop with new instructions but leave the rest.
+    prog[3] = (modr, prog[3][1], prog[4][2], prog[3][3])
+    prog[11] = (noop, 0, 0, 0)
 
 def run(iptr, prog, reg):
     assert (0 <= iptr and iptr < len(reg))
@@ -50,6 +65,7 @@ seti 9 0 5
 if __name__ == '__main__':
     test = read_input(TEST.strip())
     input = read_input(get_data(day=19, year=2018))
+    optimize(*input)
     assert(part1(*test) == 6)
     print(f'Part 1: {part1(*input)}')
     print(f'Part 2: {part2(*input)}')
