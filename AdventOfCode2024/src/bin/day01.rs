10751d56f15913dcdefe72ad20b5d3e93c3ec8eb
Alexander Utter <ooterness@gmail.com>
2024-11-30 18:24:02 -0800

    Initial project creation for 2024.

---
+++ AdventOfCode2024/src/bin/day01.rs
@@ -0,0 +1,12 @@
+/// Advent of Code 2024, Day 1
+/// Copyright 2024 by Alex Utter
+
+use aocfetch;
+
+fn main() {
+    // Fetch input from server.
+    let input = aocfetch::get_data(2024, 1).unwrap();
+
+    println!("Hello, world!");
+    println!("{}", input);
+}
